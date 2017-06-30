672369eaf7178b5d6cbb71bb6dfb0f4855048016
Gazaunga <Gazaunga@gmail.com>
2017-06-29 23:11:10 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,12 +8,7 @@ sudo modprobe uvesafb
 sudo sh -c "echo options uvesafb mode_option=1920×1080-24 scroll=ywrap > /etc/modprobe.d/uvesafb.conf"
 sudo update-initramfs -k all -u
 
-# ==> To run fbterm as a non-root user, do:
-sudo gpasswd -a gazbit video
-# ==> To enable keyboard shortcuts for non-root users, do:
-#sudo setcap 'cap_sys_tty_config+ep' /usr/bin/fbterm
-#or
-sudo chmod u+s /usr/bin/fbterm
+
 
 # cli script for pacaur + all cli apps
 pacaur -S --noconfirm --noedit diakonos \
@@ -68,6 +63,12 @@ pacaur -S --noconfirm --noedit diakonos \
 curl -sLf https://spacevim.org/install.sh | bash
 curl https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | sh
   
+  # ==> To run fbterm as a non-root user, do:
+sudo gpasswd -a gazbit video
+# ==> To enable keyboard shortcuts for non-root users, do:
+#sudo setcap 'cap_sys_tty_config+ep' /usr/bin/fbterm
+#or
+sudo chmod u+s /usr/bin/fbterm
 # rubystrap
 
 #!/bin/bash
