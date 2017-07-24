ecc4b904105e610badc623a62fc0bb5469d0f2b3
Gazaunga <Gazaunga@gmail.com>
2017-07-23 21:56:37 -0400

    Update rubystrap.sh

--- .local/bin/.scripts/rubystrap.sh
+++ .local/bin/.scripts/rubystrap.sh
@@ -29,4 +29,5 @@ cd ~
 curl https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | sh
 curl -L https://git.io/slim > ~/.zshrc
 curl -sLf https://spacevim.org/install.sh | bash
-
+mkdir -p ~/.local/share/fonts
+cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20for%20Powerline%20Nerd%20Font%20Complete.otf
